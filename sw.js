beaf587c3fe8cabcc9f7e79b2e133feae8b1dd01
maicol <181601490+maicoluniversidad@users.noreply.github.com>
2025-08-28 15:56:09 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,9 @@ const CACHE_NAME = 'rutinas-fit-cache-v1';
 const ASSETS = [
   './',
   './index.html',
-  './manifest.json'
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
 ];
 
 // Instalar y guardar en caché
